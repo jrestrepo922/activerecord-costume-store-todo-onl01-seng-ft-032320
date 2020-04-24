13972f5acbb853f0c5373baee80803fff0bd3410
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-24 18:28:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :integer
+      t.integer :size
       t.string :image_url
       t.timestamps
     end
