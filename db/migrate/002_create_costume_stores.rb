bc0b31d8a24df68b1df9ccfbeae952bb9978eed8
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-24 18:45:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores |t|
       t.string :name
       t.string :location
-      t.integer :custume_inventory
+      t.integer :costume_inventory
       t.integer :number_of_employees
       t.boolean :in_business
       t.datetime :opening_time
