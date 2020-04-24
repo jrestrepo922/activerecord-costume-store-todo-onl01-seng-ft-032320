eaf2939c1acc31f9fb27b00f77b395e22553d3c2
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-24 18:16:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores |t|
+      t.string :name
+      t.string :location
+
+    end
+  end
+end 
